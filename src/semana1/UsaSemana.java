d09dc113b10cdac5e60588a54d50fc8d133b1761
TOSHIBA <TOSHIBA@TOSHIPC>
2019-12-16 07:55:49 -0500

    Es la primera practica

---
+++ src/semana1/UsaSemana.java
@@ -0,0 +1,11 @@
+
+package semana1;
+
+
+public class UsaSemana {
+    public static void main(String[] args) {
+        Semana s=new Semana();
+        s.setVisible(true);
+        s.setLocationRelativeTo(null);
+    }
+}
